24c1be88403ebc24761c38e1bbddc57c35f5d895
Lei Zhang3554536 <Lei_Zhang3554536@jabil.com>
2024-01-30 10:50:59 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="box">
+    <div class="wave"></div>
+  </div>
+    
+</template>
+
+<style scoped>
+.box{
+  position: absolute;
+  width: 100vw;
+  top: 0;
+  left: 0;
+  /* border: 1px black solid; */
+}
+
+.wave {
+  position: relative;
+  align-items: center;
+  min-height: 100vh;
+  background-image: url(../assets/back.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow: hidden;
+}
+
+.wave:before,
+.wave:after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  min-width: 300vw;
+  min-height: 300vw;
+  background-color: #fff;
+  animation-name: rotate;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+.wave:before {
+  bottom: 80vh;
+  border-radius: 45%;
+  animation-duration: 10s;
+}
+
+.wave:after {
+  bottom: 75vh;
+  opacity: 0.5;
+  border-radius: 48%;
+  animation-duration: 10s;
+}
+
+@keyframes rotate {
+  0% {
+    transform: translate(-50%, 0) rotateZ(0deg);
+  }
+
+  50% {
+    transform: translate(-50%, 2%) rotateZ(180deg);
+  }
+
+  100% {
+    transform: translate(-50%, 0) rotateZ(360deg);
+  }
+}</style>
